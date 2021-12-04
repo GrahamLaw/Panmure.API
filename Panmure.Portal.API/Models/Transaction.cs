31fe38dbed54877b9772981815b7103acd5203b2
GrahamLaw <graham121@hotmail.com>
2021-12-04 13:11:17 +0000

    Add project files.

---
+++ Panmure.Portal.API/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Panmure.Portal.API.Models
+{
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public decimal Deposit { get; set; }
+        public decimal Withdrawl { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ Panmure.Portal.API/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Panmure.Portal.API
+{
+    public class Startup
+    {
+        readonly string CorsPolicy = "CorsPolicy";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsPolicy,
+                    builder => builder
+                        .SetIsOriginAllowed(services => true)
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                );
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseCors(CorsPolicy);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
